1f6257d80e9e155f0e18b8efbbabd879b90424fa
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2025-02-20 18:49:46 +0100

    a

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ import streamlit as st
 st.set_page_config(layout='wide')
 st.subheader('Spine project')
 
-st.subheader('12 cambios')
-st.info('Terceros cambios')
 st.image('Images/spine.png',width=500)
